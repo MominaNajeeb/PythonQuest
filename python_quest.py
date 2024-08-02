1f5d8632f11d90df1d30e098959b28a3b47804b3
Momina Najeeb <momina.najeeb@emumba.com>
2024-08-02 10:19:01 +0500

    Initial commit of python quiz app

---
+++ python_quest.py
@@ -0,0 +1,187 @@
+import csv
+username1 = "momina"
+username2 = "mahnoor"
+password1 = "najeeb"
+password2 = "iqbal"
+
+def signup():
+    new_username = input("Enter username: ")
+    new_password = input("Enter password: ")
+    print("Your account has been created! ")
+    while True:
+        username = input("Enter username: ")
+        password = input("Enter password: ")
+    
+        if username == new_username and password == new_password:
+            print("Let's get started: ")
+            get_started()
+           
+        else:
+            print("Incorrect username or password! ")
+
+def split_and_print(text):
+        strings = text.split(",")
+        for string in strings:
+            substrings = string.split("||")
+            for substring in substrings:
+                print(substring)
+
+def get_started():
+    count_easy= 0
+    count_medium = 0
+    count_hard = 0
+    score = 0
+    difficulty = int(input("Select difficulty level:\n" + "1.Easy\n" + "2.Medium\n" + "3.Hard\n"))  
+    if difficulty == 1:
+
+        with open("quiz_data.csv", 'r') as csvfile:
+            reader = csv.reader(csvfile)
+            next(reader)
+            
+            for row in reader:
+                if count_easy<10:
+                    split_and_print(row[0])
+                    question = input("Your answer: ")
+                    if question == row[1]:
+                        print("Your answer is correct! ✅\n", "You earned 2 points! 🎉\n")
+                        score += 2
+                        print("Your current score:", score)
+                    
+                    else:
+                        print("Oops, that's not right! ❌")
+                        print("Your current score:", score)
+
+                    count_easy += 1    
+
+                else:
+                    break    
+
+        print("You earned:", str(score), "points")
+        if score >= 18 and score <=20:
+            print("You are a Superstar of Python!")
+
+        elif score >= 14 and score <=17:
+            print("You are a Star Performer!")
+
+        elif score >=10 and score <=13:
+            print("You are a Solid Achiever!")
+            
+        elif score >= 6 and score <=9:
+            print("You are a Rising Star of Python!")  
+
+        else:
+            score >= 18 and score <=20
+            print("You are a Learning Explorer of Python")
+
+    elif difficulty == 2:
+        with open("quiz_data.csv", 'r') as csvfile:
+            reader = csv.reader(csvfile)
+            
+            for row in reader:
+                if count_medium>10 and count_medium<21:
+                    split_and_print(row[0])
+                    question = input("Your answer: ")
+                    if question == row[1]:
+                        print("Your answer is correct! ✅\n", "You earned 2 points! 🎉\n")
+                        score += 2
+                        print("Your current score:", score)
+                    
+                    else:
+                        print("Oops, that's not right! ❌")
+                        print("Your current score:", score)
+
+                    count_medium += 1    
+
+                else:
+                    count_medium += 1 
+
+        print("You earned:", str(score), "points")
+        if score >= 18 and score <=20:
+            print("You are a Superstar of Python!")
+
+        elif score >= 14 and score <=17:
+            print("You are a Star Performer!")
+
+        elif score >=10 and score <=13:
+            print("You are a Solid Achiever!")
+            
+        elif score >= 6 and score <=9:
+            print("You are a Rising Star of Python!")  
+
+        else:
+            score >= 18 and score <=20
+            print("You are a Learning Explorer of Python")
+
+    elif difficulty == 3:
+        with open("quiz_data.csv", 'r') as csvfile:
+            reader = csv.reader(csvfile)
+            
+            for row in reader:
+                if count_hard>20 and count_hard<31:
+                    split_and_print(row[0])
+                    question = input("Your answer: ")
+                    if question == row[1]:
+                        print("Your answer is correct! ✅\n", "You earned 2 points! 🎉\n")
+                        score += 2
+                        print("Your current score:", score)
+                    
+                    else:
+                        print("Oops, that's not right! ❌")
+                        print("Your current score:", score)
+
+                    count_hard += 1    
+
+                else:
+                    count_hard += 1    
+
+        print("You earned:", str(score), "points")
+        if score >= 18 and score <=20:
+            print("You are a Superstar of Python! 🌟")
+
+        elif score >= 14 and score <=17:
+            print("You are a Star Performer! 🌟")
+
+        elif score >=10 and score <=13:
+            print("You are a Solid Achiever! ⭐")
+            
+        elif score >= 6 and score <=9:
+            print("You are a Rising Star of Python! ⭐")  
+
+        else:
+            score >= 18 and score <=20
+            print("You are a Learning Explorer of Python! ⭐")
+
+    else:
+        print("Please enter a valid difficulty level! ")                
+
+
+print("******************************************************** WELCOME TO PYTHON QUEST! 🐍*******************************************************")
+print("Welcome to Python Quest! 🐍 Test your Python skills with quizzes of varying difficulty levels—Easy, Medium, or Hard. Sign up or log in to track your progress, answer multiple-choice questions, and earn titles based on your score. Review your results, improve your knowledge, and enjoy the journey of mastering Python. Start your Python Quest today and level up your coding expertise!")
+
+while True:
+    start = int(input("1. LOGIN\n" + "2. SIGNUP\n" + "3. Choose one: "))
+
+    if start == 1:
+        username = input("Enter username: ")
+        password = input("Enter password: ")
+        if username == username1 and password == password1:
+            print("Welcome back!")
+            print("Let's get started!")
+            get_started()
+        elif username == username2 and password == password2:
+            print("Welcome back!")
+            print("Let's get started!")
+            get_started()
+        else:    
+            print("Incorrect username or password! ")
+            print("Create account: ")
+            signup()
+
+    elif start == 2:
+        signup()        
+
+    else:
+        print("Invalid attempt! ")
+
+ 
+
